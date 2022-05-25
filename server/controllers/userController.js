ce9db64b761e42f41bedf29818e68a8ca54eb6c4
TimTi01 <TimF1234@yandex.ru>
2022-05-25 17:19:30 +0300

    added registration with hash password and jwt-token in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,32 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, // первый объект - центральная часть jwt-токена
+        process.env.SECRET_KEY, // второй параметр - секретный ключ
+        {expiresIn: '24h'} // 3 параметр - опции. Одна из опции, это время жизни токена
+    )
+}
 
 class UserController {
-    async registration(req, res) {
-
+    async registration(req, res, next) {
+        // Передача email, пароля и роли. Роль требуется для отдельного создания пользователей и админов 
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или password!'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5) // хеширование пароля при помощи функции hash. В данном примере хешируем 5 раз
+        const user = await User.create({email, role, password: hashPassword}) // создание пользователя
+        const basket = await Basket.create({userId: user.id}) // создание корзины пользователя
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async login(req, res) {
